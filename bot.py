6b69040a3b5637750c312a4406f6fc6baf4e8391
dzshadowone <dz.shadow.one@gmail.com>
2026-06-23 18:16:24 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
+import requests
+import os
 
+print("BOT STARTED")
+
+webhook = os.environ.get("WEBHOOK_URL")
+
+print("WEBHOOK:", webhook)
+
+if not webhook:
+    print("ERROR: WEBHOOK_URL is missing")
+    exit()
+
+print("Sending Discord message...")
+
+response = requests.post(webhook, json={
+    "content": "✅ TEST: bot is working correctly"
+})
+
+print("STATUS:", response.status_code)
+print("RESPONSE:", response.text)
