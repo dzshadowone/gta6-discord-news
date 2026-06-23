480bf2edc94fa3142da808afb3984a436481d5be
dzshadowone <dz.shadow.one@gmail.com>
2026-06-23 17:57:44 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,8 @@
 import requests
-import feedparser
 import os
 
 WEBHOOK = os.environ["WEBHOOK_URL"]
 
-FEEDS = [
-    "https://www.gamespot.com/feeds/mashup/",
-    "https://www.ign.com/rss/articles",
-]
-
-def send_discord(title, link):
-    data = {
-        "content": f"🚨 GTA 6 Update:\n**{title}**\n{link}"
-    }
-    requests.post(WEBHOOK, json=data)
-
-for url in FEEDS:
-    feed = feedparser.parse(url)
-
-    for entry in feed.entries[:3]:
-        if "gta" in entry.title.lower() or "rockstar" in entry.title.lower():
-            send_discord(entry.title, entry.link)
+requests.post(WEBHOOK, json={
+    "content": "✅ TEST: Discord webhook is working"
+})
