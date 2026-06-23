c411c0dd29a8ed53caf294396115e87c4d22a821
dzshadowone <dz.shadow.one@gmail.com>
2026-06-23 18:15:52 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,1 @@
-import requests
-import os
 
-WEBHOOK = os.environ["WEBHOOK_URL"]
-
-print("Sending test message...")
-
-r = requests.post(WEBHOOK, json={
-    "content": "✅ TEST MESSAGE: webhook is working"
-})
-
-print("Status code:", r.status_code)
-print("Response:", r.text)
